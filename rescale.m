083f6966f773a404ec742706804e8f468c7a7872
Raul Hendriks <rghendriks@tudelft.nl>
2026-06-08 12:40:32 +0200

    retuned Kalman propperly

---
+++ rescale.m
@@ -0,0 +1,23 @@
+function y = rescale(u)
+th1in = u(1);
+th2in = u(2);
+
+scale1 = 4.881666285191120;
+scale2 = 4.932936598764020;
+
+% eq1 = 3.796622000341846/scale1 *2*pi;
+% eq2 = 1.210150569834988/scale2 *2*pi+pi;
+%down up
+% eq1 = 3.806622000341846/scale1 *2*pi;
+% eq2 = 1.189150569834988/scale2 *2*pi + pi + 2*pi/360*7.5;
+deg_to_add = 0;
+%up up
+eq1 = 3.806622000341846/scale1 *2*pi+ pi - 2*pi/360*deg_to_add;
+eq2 = 1.189150569834988/scale2 *2*pi - 2*pi/360*10 + 2*pi/360*deg_to_add; %low 7
+
+
+th1 = mod(th1in/scale1*2*pi - eq1 +pi, 2*pi) -pi;
+th2 = mod(th2in/scale2*2*pi - eq2 +pi, 2*pi) -pi;
+
+y = [th1
+     th2];
