9c4327b66ae73f822a5bdec74f30689614c24804
Raul <hendriks.raul@gmail.com>
2026-06-16 14:04:40 +0200

    Changed labels

--- Plotting_data.m
+++ Plotting_data.m
@@ -1,7 +1,7 @@
 % Load the data
-dataStruct = load('LQR_up_up_ref_trac.mat');
+dataStruct = load('MPC_up_up_equili_dist.mat');
 data = dataStruct.ans';
-% data = data(1:500, :);
+data = data(150:350, :);
 
 % Extract columns
 time         = data(:, 1);
@@ -26,7 +26,7 @@ yline(1, 'r--', 'Max (+1)', 'LineWidth', 1.5, 'LabelHorizontalAlignment', 'left'
 yline(-1, 'r--', 'Min (-1)', 'LineWidth', 1.5, 'LabelHorizontalAlignment', 'left');
 hold off;
 ylim([-1.2, 1.2]); % Force bounds slightly wider than saturation
-ylabel('Torque / Input', 'FontWeight', 'bold');
+ylabel('Input', 'FontWeight', 'bold');
 legend('Control Effort', 'Location', 'northeast');
 grid on; set(gca, 'XTickLabel', []);
 
@@ -38,7 +38,7 @@ plot(time, meas_1, 'LineWidth', 1.5, 'Color', [0 0.4470 0.7410]);
 plot(time, est_1, '-.', 'LineWidth', 1.5, 'Color', [0.8500 0.3250 0.0980]);
 hold off;
 ylim([-1, 1]);
-ylabel('Link 1 Angle', 'FontWeight', 'bold');
+ylabel('Link 1 Angle (rad)', 'FontWeight', 'bold');
 legend('Reference', 'Measured', 'Estimated', 'Location', 'northeast');
 grid on; set(gca, 'XTickLabel', []);
 
@@ -49,7 +49,7 @@ plot(time, meas_2_abs, 'LineWidth', 1.5, 'Color', [0.4660 0.6740 0.1880]);
 plot(time, est_2, '-.', 'LineWidth', 1.5, 'Color', [0.6350 0.0780 0.1840]);
 hold off;
 ylim([-0.1, 0.1]);
-ylabel('Link 2 Angle', 'FontWeight', 'bold');
+ylabel('Link 2 Angle (rad)', 'FontWeight', 'bold');
 legend('Measured (abs)', 'Estimated', 'Location', 'northeast');
 grid on; set(gca, 'XTickLabel', []);
 
@@ -61,9 +61,9 @@ plot(time, est_speed_2, 'LineWidth', 1.5);
 hold off;
 ylim([-2, 2]);
 xlabel('Time', 'FontWeight', 'bold');
-ylabel('Angular Vel', 'FontWeight', 'bold');
+ylabel('Angular Vel (rad/s)', 'FontWeight', 'bold');
 legend('Est Velocity 1', 'Est Velocity 2', 'Location', 'northeast');
 grid on;
 
 % Add overall title
-sgtitle('Double Pendulum MPC: Up-Up Equilibrium Hold', 'FontSize', 15, 'FontWeight', 'bold');
+sgtitle('Double Pendulum MPC: Up-Up Equilibrium disturbed', 'FontSize', 15, 'FontWeight', 'bold');
